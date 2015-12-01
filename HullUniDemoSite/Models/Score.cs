7bb65ce0a057fc2df6be0053e29a4a6ca3946eaf
Sarkimedes <peter.w.roberts93@gmail.com>
2015-12-01 15:20:47 +0000

    Added DB and controllers

---
+++ HullUniDemoSite/Models/Score.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HullUniDemoSite.Models
+{
+    public class Score
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public int Moves { get; set;  }
+    }
+}
---
+++ HullUniDemoSite/Models/SpaceCheeseDb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HullUniDemoSite.Models
+{
+    public class SpaceCheeseDb : DbContext
+    {
+        public  DbSet<Score> Scores { get; set; }
+    }
+}
